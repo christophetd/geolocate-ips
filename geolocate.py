d5a5bc6cd46e687cc0c65a79e267ba0df64da20a
Christophe Tafani-Dereeper <christophetd@hotmail.fr>
2017-07-13 14:11:33 +0200

    Add better error handling

--- geolocate.py
+++ geolocate.py
@@ -19,12 +19,14 @@ def read_ip_list():
         exit(1)
 
 def get_ip_country(ip):
-    response = requests.get('http://ip2c.org/{}'.format(ip));
-    if response.status_code != 200:
-        sys.stderr.write('Could not retrieve any geolocation info for IP {} - skipping.\n'.format(ip))
-        return False
+    response = requests.get('http://ip2c.org/{}'.format(ip))
+    error_codes = [ 'WRONG INPUT', 'UNKNOWN' ] # http://about.ip2c.org/#outputs
 
     country_name = response.text.split(';')[3]
+
+    if response.status_code != 200 or country_name in error_codes:
+        sys.stderr.write('Could not retrieve any geolocation info for IP {} - skipping.\n'.format(ip))
+        return False
 
     if country_name == 'Reserved':
         sys.stderr.write('Could not retrieve any geolocation for IP {} - it looks like a private address\n'.format(ip))
